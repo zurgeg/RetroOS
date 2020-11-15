357f829c006ecb1c1dac55a4356b8cf49dbdada5
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-15 09:59:04 -0500

    Create kernel.c

---
+++ kernel.c
@@ -0,0 +1,10 @@
+void dummy_test_entrypoint() {
+}
+
+void retro_print_string(string) {
+    char* video_memory = (char*) 0xb8000;
+    *video_memory = string;
+    return 0;
+void main() {
+    retro_print_string('Welcome to RetroOS')
+}
