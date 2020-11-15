a121e962baddc4667fd6ce40b5e2aca89694e7a0
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-15 09:59:37 -0500

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,10 +1,4 @@
-mov ah, 0x0e
-int 0x10
-mov bx, welcome
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
-welcome:
-    ; ASCII code 0x58 ('X') is stored just before the zero-padding.
-    ; On this code that is at byte 0x2d (check it out using 'xxd file.bin')
-    db "Welcome to RetroOS."
+[bits 32]
+[extern main]
+call main
+jmp $
